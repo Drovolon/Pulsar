7178cd3165a7a428072c14a0688cc3bdaba37621
Drovolon <me@drovolon.org>
2026-06-27 20:23:58 -0700

    Initial commit: fork from SamplePlugin, rename to Pulsar

---
+++ Pulsar/Windows/MainWindow.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Numerics;
+using Dalamud.Bindings.ImGui;
+using Dalamud.Interface.Utility.Raii;
+using Dalamud.Interface.Windowing;
+
+namespace Pulsar.Windows;
+
+public class MainWindow : Window, IDisposable
+{
+    private readonly Plugin plugin;
+
+    public MainWindow(Plugin plugin)
+        : base("Pulsar##pulsar", ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse)
+    {
+        SizeConstraints = new WindowSizeConstraints
+        {
+            MinimumSize = new Vector2(375, 330),
+            MaximumSize = new Vector2(float.MaxValue, float.MaxValue)
+        };
+
+        this.plugin = plugin;
+    }
+
+    public void Dispose() { }
+
+    public override void Draw()
+    {
+        ImGui.Text($"The random config bool is {plugin.Configuration.SomePropertyToBeSavedAndWithADefault}");
+
+        if (ImGui.Button("Show Settings"))
+        {
+            plugin.ToggleConfigUi();
+        }
+
+        ImGui.Spacing();
+
+        using var child = ImRaii.Child("SomeChildWithAScrollbar", Vector2.Zero, true);
+
+        if (child.Success)
+        {
+            ImGui.TextUnformatted("Hello, world!");
+        }
+    }
+}
